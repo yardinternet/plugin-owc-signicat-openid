75a28275e3691280530070973aa08d4468c9071f
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2024-04-29 09:55:11 +0200

    (feat) update GF fields

--- src/Interfaces/Services/GravityFormsServiceInterface.php
+++ src/Interfaces/Services/GravityFormsServiceInterface.php
@@ -2,13 +2,6 @@
 
 namespace OWCSignicatOpenID\Interfaces\Services;
 
-/**
- * Exit when accessed directly.
- */
-if (! defined('ABSPATH')) {
-    exit;
-}
-
 interface GravityFormsServiceInterface
 {
 
