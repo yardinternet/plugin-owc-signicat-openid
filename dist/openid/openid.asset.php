11d42fbae0749332d7c65efeda0d9bcef43ee2ac
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2024-05-16 11:45:19 +0200

    fixes

--- dist/openid/openid.asset.php
+++ dist/openid/openid.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '75a57fa4668148b63e30');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'f7abe42343ecb7b003c9');
