7d19c607dd0b335c653487b7a09c835db2ee433f
YvetteNikolov <yvettenikolov@gmail.com>
2024-07-10 15:59:16 +0200

    (refractor): refractor block to only output a link

--- dist/openid/openid.asset.php
+++ dist/openid/openid.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'f7abe42343ecb7b003c9');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '4e9fc999fcc7f7a1c495');
