ad35ad6325c50f01211a0bda235669611b6d9b4d
YvetteNikolov <yvettenikolov@gmail.com>
2024-09-02 15:52:10 +0200

    (feat): add timer to modal

--- resources/views/modal.php
+++ resources/views/modal.php
@@ -2,14 +2,14 @@
 	<div id='owc-signicat-openid-modal-dialog' class='modal-dialog' role='document'>
 		<div class='modal-content'>
 			<div class='modal-header'>
-				<h5 class='modal-title'>Uw sessie verloopt.</h5>
+				<h2 class='modal-title'>Uw sessie verloopt.</h2>
 			</div>
 			<div class='modal-body | mb-4'>
-				Uw sessie is mogelijk verlopen. Als u te lang niks hebt gedaan, wordt u uit veiligheidsoverwegingen uitgelogd.
-				Kies 'Verlengen' om uw sessie te verlengen, mogelijk moet u opnieuw inloggen.
+				Vanwege veiligheidsredenen wordt u automatisch uitgelogd na een periode van inactiviteit. Wilt u uw sessie behouden? Klik dan op 'Verlengen'.
 			</div>
-			<div class='modal-footer | d-flex justify-content-end' >
-				<button type='submit' id='owc-signicat-openid-logout' tabindex='0' role='button' class='btn btn-outline-primary mr-2' data-dismiss='modal'>Sluiten</button>
+			<div class='modal-footer | d-flex justify-content-between align-items-end'>
+				<div id='owc-signicat-openid-modal-timer' class='owc-signicat-openid-modal-timer'></div>
+				<button type='submit' id='owc-signicat-openid-logout' tabindex='0' role='button' class='ml-auto mr-2 btn btn-outline-primary' data-dismiss='modal'>Uitloggen</button>
 				<button type='button' id='owc-signicat-openid-refresh' tabindex='0' role='button' class='btn btn-primary'>Verlengen</button>
 			</div>
 		</div>
